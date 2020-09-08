f6e97c1ce74e79733c403c990f989615edeaccc5
Matteus Colins <matteusc.moreira@gmail.com>
2020-09-08 15:06:13 -0300

    First configuration

--- src/main/java/com/mycompany/dropwizard/App.java
+++ src/main/java/com/mycompany/dropwizard/App.java
@@ -1,13 +1,35 @@
 package com.mycompany.dropwizard;
 
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application<HelloWorldConfiguration>
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+        new App().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    /**
+     * Configura determinados aspectos da aplicação antes de executar
+     * @param bootstrap
+     */
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration helloWorldConfiguration, Environment environment) throws Exception {
+
     }
 }
